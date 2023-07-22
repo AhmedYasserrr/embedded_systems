fcf654d1cc127f6dcd0b69387477a7549fa5c959
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-07-22 20:31:12 +0300

    Second commit

--- c programming/S1/main.c
+++ c programming/S1/main.c
@@ -3,11 +3,12 @@
 int set(int n, int pos){return (n | (1<<pos));}
 int toggle(int n, int pos){return (n ^ (1<<pos));}
 int clear(int n, int pos){return (n & ~(1<<pos));}
+int read(int n, int pos){return (1 & (n>>pos));}
 
 int main(void){
     int n; int pos;
     scanf("%d %d", &n, &pos);
-    printf("set bit %d to 0: %d\n", pos, clear(n, pos));
+    printf("read bit %d: %d\n", pos, read(n, pos));
     printf("set bit %d to 0: %d\n", pos, clear(n, pos));
     printf("set bit %d to 1: %d\n", pos, set(n, pos));
     printf("toggle bit %d: %d\n", pos, toggle(n, pos));

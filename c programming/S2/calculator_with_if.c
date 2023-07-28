2d24e7a910e3130268a56cc3e3a13a1bbbb47d11
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-07-28 20:51:38 +0300

    conditions

---
+++ c programming/S2/calculator_with_if.c
@@ -0,0 +1,25 @@
+/************************************************************
+* Filename: calculator
+* Author: Ahmed Yasser
+* Date: 28 / 7 / 2023
+************************************************************/
+#include <stdio.h>
+
+
+int main(void){
+    char op;
+    float n1; float n2;
+    printf("Welcome in calculator app\n");
+    printf("Enter: type of operation  num1 num2\n");
+    printf("1 for +, 2 for -, 3 for *, 4 for / \n");
+    scanf(" %c %f %f",&op, &n1, &n2);
+    if(op == '1'){printf("%f", n1+n2);}
+    else if(op == '2'){printf("%f", n1-n2);}
+    else if(op == '3'){printf("%f", n1*n2);}
+    else if(op == '4'){
+        if(n2 == 0) printf("this is not applicable");
+        else printf("%f", (double)(n1) / n2);
+        }
+    else {}
+    return 0;
+}
---
+++ c programming/S2/calculator_with_switch.c
@@ -0,0 +1,44 @@
+/************************************************************
+* Filename: calculator_with_switch
+* Author: Ahmed Yasser
+* Date: 28 / 7 / 2023
+************************************************************/
+#include <stdio.h>
+
+int main(void){
+    char op;
+    float n1; float n2;
+    printf("Welcome in calculator app\n");
+    printf("Enter: type of operation  num1 num2\n");
+    printf("1 for +, 2 for -, 3 for *, 4 for / \n");
+    scanf(" %c %f %f",&op, &n1, &n2);
+
+while(op != -1){
+    switch(op){
+        case '1':
+            printf("%f", n1+n2); op = -1;
+            break;
+        case '2':
+            printf("%f", n1-n2); op = -1;
+            break;
+        case '3':
+            printf("%f", n1*n2); op = -1;
+            break;
+        case '4':
+                if(n2 == 0){ printf("this is not applicable\n"); 
+                op = '5';}
+                else {printf("%f", (double)(n1) / n2); op = -1;}
+            break; 
+        case '5':
+                printf("Enter: type of operation  num1 num2\n");
+                printf("1 for +, 2 for -, 3 for *, 4 for / \n");
+                scanf(" %c %f %f",&op, &n1, &n2);
+                break;
+        default:
+            printf("this is not an operation\n");
+            op = '5';
+            break;
+    }
+}
+    return 0;
+}

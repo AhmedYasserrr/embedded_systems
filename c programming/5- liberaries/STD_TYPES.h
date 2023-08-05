daa85486de8d56b309ab50d7dddc56bfa9dd30c5
AhmedYasserrr <ahmdyasrj@gmail.com>
2023-08-05 19:54:42 +0300

    headers

---
+++ c programming/5- liberaries/STD_TYPES.h
@@ -0,0 +1,8 @@
+typedef char                sint8;
+typedef unsigned char       uint8;
+typedef short int           sint16;
+typedef unsigned short int  uint16;
+typedef long int            sint32;
+typedef unsigned long int   uint32;
+typedef float               float32;
+typedef double              float64;
---
+++ c programming/5- liberaries/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "STD_TYPES.h"
+
+int main(){
+    uint32 y = 10;
+    printf("%d", y);
+}
